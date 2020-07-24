ea3e3e32d287bb2df3ca4cef9db93332cc5da7a8
Nicholas Kerrigan <38679568+kerrnick50@users.noreply.github.com>
2020-07-23 19:50:37 -0500

    Update Fixedsamplecode.py

--- Fixedsamplecode/Fixedsamplecode/Fixedsamplecode.py
+++ Fixedsamplecode/Fixedsamplecode/Fixedsamplecode.py
@@ -13,7 +13,6 @@ def displayIntro():
 	you see two caves. In one cave, the dragon is friendly
 	and will share his treasure with you. The other dragon
 	is greedy and hungry, and will eat you on sight.''')
-	print()
 
 def chooseCave():
 	cave = ''
