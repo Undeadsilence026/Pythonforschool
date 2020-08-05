f40a0e88d56305929e2ebbbeae1e59cfbe4b208c
Nicholas Kerrigan <38679568+kerrnick50@users.noreply.github.com>
2020-08-05 16:24:46 -0500

    Add files via upload
    Fixed issues with code and fixed up some of the wording

--- Assignment 9/Assignment 9/Assignment_9.py
+++ Assignment 9/Assignment 9/Assignment_9.py
@@ -4,17 +4,17 @@ import os
 import time
 import logging
 
-
 def chooseDir():
 	print("This is your current file directory "+os.getcwd())
 	time.sleep(2)
-	print("Is there another file directory would you like to use?")
+	print("Is there another file directory would you like to use?\nIf not copy and paste the current directory.")
 	time.sleep(2)
 	print("Remember that file directories are case sensitive")
 	user_input = input()
-	answer = input("Is this the file directory you want? "+user_input)
-	os.chdir(user_input)
+	answer = input("Is this the file directory you want? "+user_input+"\n")
+	#os.chdir(user_input)
 	if answer == 'yes' or answer == 'y':
+		os.chdir(user_input)
 		pass
 	else:
 		print("Enter the correct file directory")
@@ -22,16 +22,14 @@ def chooseDir():
 		os.chdir(user_input)
 
 def creatingFiles():
-	print("Is there a specific file you would like to place your text in?")
-	time.sleep(2)
-	print("Would you like to create a new file?")
+	print("Create a new folder to place your text in")
 	time.sleep(2)
 	print("Remember that files can also be case sensitive")
-	time.sleep(2)
-	#print("Use the format \Filename")
 	user_input2 = input()
-	print("Creating file "+user_input2)
+	print("Creating Folder "+user_input2)
 	os.mkdir(user_input2)
+	os.chdir(user_input2)
+	#logging.basicConfig(filename=user_input2, level=logging.DEBUG)
 
 def loggingData():
 	print("Please log your first and last name")
@@ -42,8 +40,8 @@ def loggingData():
 	name = input()
 	address = input()
 	phoneNumber = input()
-	print("You have entered "+name+' '+' '+address+' '+phoneNumber)
-	answer2 = input("Is this information correct")
+	print("You have entered\n"+name+'\n'+address+'\n'+phoneNumber)
+	answer2 = input("Is this information correct\n")
 	if answer2 == 'yes' or answer2 == 'y':
 		pass
 	else:
@@ -51,16 +49,18 @@ def loggingData():
 		name = input()
 		address = input()
 		phoneNumber = input()
-	logging.basicConfig(filename=user_input2+'.txt', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
-	logging.debug = name
-	logging.debug = address
-	logging.debug = phoneNumber
+		print("You have entered\n"+name+'\n'+address+'\n'+phoneNumber)
+	user_input3 = input("What is the name of the file your placing the text in?\n")
+	logging.basicConfig(filename=user_input3+".txt", level=logging.DEBUG)
+	logging.debug(name)
+	logging.debug(address)
+	logging.debug(phoneNumber)
 
 tryAgain = 'yes'
 while tryAgain == 'yes' or tryAgain == 'y':
 	chooseDir()
 	creatingFiles()
-	loggingData(creatingFiles)
+	loggingData()
 	print("Would you like to try again? (yes/no)")
 	tryAgain = input()
 	if tryAgain == 'no':
